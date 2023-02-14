17f4dd5ea420d807c85da9909c4d1bd8d9c0db0c
eliton-dev <eliton.assis@bel.ind.br>
2023-02-14 12:12:09 -0300

    melhoria

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//class criada pelo chatGpt
+window.onload = run();
+function run() {
+    console.log('carregado')
+}
+
 
 class Speech {
     constructor(text, lang = 'pt-BR', pitch = 1, rate = 1, volume = 3) {
@@ -16,81 +22,87 @@ class Speech {
         fala.volume = this.volume;
         speechSynthesis.speak(fala)
     }
+    capture() {
+        const recognition = new webkitSpeechRecognition();
+        recognition.lang = this.lang;
+        recognition.start();
+        recognition.onresult = (event) => {
+            const text = event.results[0][0].transcript;
+            console.log(text);
+        };
+    }
+
 }
 
+debugger
 
-
-let previousTxt;
-
-
-let timerId;
-
+let previousTxt
 let parar = false
+let btn
+setTimeout(() => {
+    btn = document.querySelector('textarea').parentElement.querySelector('button')
+    console.log(btn)
+    btn.addEventListener('click', () => {
+        console.log('btn clicked')
+        previousTxt = null
+        parar = false
+        getVoice()
+    })
+}, 2000)
 
-let btn = document.querySelector('textarea').parentElement.querySelector('button')
+// while (btn == null) {
+//     getBtn()
+// }
 
-let input = document.createElement('input')
+// function setEvent() {
+//     console.log('btn clicked')
+//     previousTxt = null
+//     parar = false
+//     getVoice()
+// }
+// function getBtn() {
+//     btn = document.querySelector('textarea').parentElement.querySelector('button')
+//     console.log(btn)
 
-input.setAttribute('type', 'text')
+//     if (!!btn) {
+//         btn.onclick = setEvent
+//         return
+//     } else
+//         getBtn()
+// }
+
+// function captureVoice() {
+//     const input = document.querySelector('textarea');
+//     console.log(input)
+// }
 
 
-function teste() {
+
+
+function getVoice() {
     setTimeout(() => {
         if (parar) return;
 
         let divs = document.querySelectorAll('.markdown');
-
         if (divs.length > 0) {
+            txt = divs[divs.length - 1]?.firstChild.innerHTML;
 
-            let txt = divs[divs.length - 1]?.firstChild.innerHTML;
-
-            if (!!txt && txt.trim() != "" && txt !== previousTxt) {
-
-                console.log(txt, 'valor txt')
-                previousTxt = txt;
-
-            } else {
-                input.value = txt;
-                parar = true;
-                new Speech(txt).start();
+            if (!!txt && !/^\s*$/.test(txt)) {
+                if (txt !== previousTxt)
+                    previousTxt = txt
+                else {
+                    parar = true;
+                    console.log(txt)
+                    new Speech(txt).start();
+                }
             }
         }
-
-        if (!parar) teste();
-    }, 1000);
+        if (!parar) getVoice();
+    }, 1250);
 }
 
 
 
-// function voz() {
-//     console.log(input.value,'valor do input')
-//     clearTimeout(timerId);
-//     timerId = setTimeout(() => {
-//         parar = true;
-//         new Speech(input.value).start();
-//         input.value = ""
-//     }, 1000);
-// }
-
-btn.addEventListener('click', () => {
-    parar = false
-    teste()
-
-  //  input.addEventListener("input", voz)
-})
-
-// clica 
-// pega valor da div 
-// se div tem valores repetidos a cada 1 segundos para
-// quando parar input recebe valor e dispara evento || chama metodo da class
-
-
-
-
-
-
-
-// a cada espaço na frase add a palavra em um array da uma for jogando palavra por palavra no metodo da class
 
 
 
